3cf84d5c9c7a7adb050cfead64b1f9e50135862e
ThanhTrieuVo <ttrieu@lunageo.com>
2026-04-17 17:05:23 -0400

    Added Footer component

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Header } from './client/layout/header/header';
 import { Map } from "./shared/map/map";
 import { Carousel } from "./client/features/carousel/carousel";
+import { Footer } from './client/layout/footer/footer';
 
 
 @Component({
   selector: 'app-root',
-  imports: [Header, Map, Carousel],
+  imports: [Header, Map, Carousel, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
