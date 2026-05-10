0d91c3e3523d360794de83e345b79d54a31b5732
ThanhTrieuVo <ttrieu@lunageo.com>
2026-05-10 01:09:02 -0400

    Showed Reviews on Client home page

--- Frontend/src/app/client/features/home/home.ts
+++ Frontend/src/app/client/features/home/home.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { finalize } from 'rxjs';
 import { Carousel } from "../carousel/carousel";
 import { Map } from "../../../shared/map/map";
+import { ReviewCard } from "../../components/review-card/review-card";
+import { ReviewsService } from "../../../core/services/reviews-service";
 
 @Component({
   selector: 'app-home',
-  imports: [Carousel, Map],
+  imports: [Carousel, Map, ReviewCard, RouterLink],
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home implements OnInit {
+  private reviewsService = inject(ReviewsService);
+
+  reviews = this.reviewsService.reviews;
+  homeReviews = computed(() => this.reviews().slice(0, 5));
+  loadingReviews = signal(false);
+  reviewsError = signal(false);
+
+  ngOnInit() {
+    this.loadReviews();
+  }
+
+  private loadReviews() {
+    this.loadingReviews.set(true);
+    this.reviewsError.set(false);
+
+    this.reviewsService
+      .loadReviews({
+        pageNumber: 1,
+        pageSize: 6,
+        sortBy: 'created',
+        sortDirection: 'desc',
+      })
+      .pipe(finalize(() => this.loadingReviews.set(false)))
+      .subscribe({
+        error: () => this.reviewsError.set(true),
+      });
+  }
+}
